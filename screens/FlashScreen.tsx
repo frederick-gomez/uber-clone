82978c991f95fe495fe02acf073c91cc8a64de0a
Fede Gómez <gomez.fede98@gmail.com>
2022-08-04 18:10:56 -0400

    Flash screen content added

--- screens/FlashScreen.tsx
+++ screens/FlashScreen.tsx
@@ -1,14 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { Icon } from '@rneui/themed';
 
 const FlashScreen = () => {
 	return (
-		<View>
-			<Text>FlashScreen</Text>
+		<View style={styles.container}>
+			<Icon name='engineering' type='material' reverse color='#000' size={100} />
+			<Text style={styles.text}>Bajo construcción...</Text>
 		</View>
 	);
 };
 
 export default FlashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	text: {
+		fontSize: 26,
+		fontWeight: '600',
+		marginTop: 30,
+	},
+});
